77524e94c1c345ca3303abfe5c25623907620808
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-12-30 21:14:25 +0900

    fix(wezterm): remove ctrl+"/" and replace ctrl+shift with shift to avoid multiple actions to the same key

--- wezterm/config/bindings.lua
+++ wezterm/config/bindings.lua
@@ -1,8 +1,7 @@
 local wezterm = require("wezterm")
 return {
-	{ key = "C", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
-	{ key = "V", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "C", mods = "SHIFT", action = wezterm.action.CopyTo("Clipboard") },
+	{ key = "V", mods = "SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
 	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
 	{ key = "^", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-	{ key = "/", mods = "CTRL", action = wezterm.action.SendString("\x1f") },
 }
