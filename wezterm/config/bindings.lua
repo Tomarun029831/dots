b45e48d871ad1290669a65b6ea0ea0bbd1c04530
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-01-02 20:15:27 +0900

    fix(wezterm): redefine keybindings in wezterm to avoid a confilct of ones in neovim

--- wezterm/config/bindings.lua
+++ wezterm/config/bindings.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 return {
-	{ key = "C", mods = "SHIFT", action = wezterm.action.CopyTo("Clipboard") },
-	{ key = "V", mods = "SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+	{ key = "C", mods = "SHIFT|ALT", action = wezterm.action.CopyTo("Clipboard") },
+	{ key = "V", mods = "SHIFT|ALT", action = wezterm.action.PasteFrom("Clipboard") },
 	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
 	{ key = "^", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
 }
