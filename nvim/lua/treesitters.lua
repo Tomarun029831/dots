296014eec9f562df19d0be5f8733085b3993becc
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-08-03 12:33:40 +0900

    chore(nvim): disable code-folding

--- nvim/lua/treesitters.lua
+++ nvim/lua/treesitters.lua
@@ -5,8 +5,7 @@ vim.api.nvim_create_autocmd('FileType', {
 		pattern = filetypes,
 		callback = function()
 				vim.treesitter.start()
-				vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-				vim.wo.foldmethod = 'expr'
+				vim.wo.foldenable = false
 				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end,
 })
