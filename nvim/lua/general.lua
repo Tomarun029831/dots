3693885d9c928cff9bbf1b3c4bc86bb812d9c1ee
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-08-03 12:56:00 +0900

    feat(nvim): set shell to open nushell

--- nvim/lua/general.lua
+++ nvim/lua/general.lua
@@ -10,6 +10,7 @@ vim.o.termguicolors = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = false
+vim.o.shell = 'nu'
 vim.o.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
